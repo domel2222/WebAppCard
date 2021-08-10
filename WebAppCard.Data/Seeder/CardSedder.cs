82c21e5f644a882e7b6db5cae81d381473d54ed3
Dominik Wikliński <domel2222@gmail.com>
2021-08-10 12:31:05 +0200

    changed seed into seed async

--- WebAppCard.Data/Seeder/CardSedder.cs
+++ WebAppCard.Data/Seeder/CardSedder.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,16 +14,43 @@ namespace WebAppCard.Data.Seeder
     public class CardSedder
     {
         private readonly CardContext _cardContext;
+        private readonly UserManager<StoreUser> _userManager;
 
-        public CardSedder(CardContext cardContext)
+        public CardSedder(CardContext cardContext,
+                            UserManager<StoreUser> userManager)
         {
             this._cardContext = cardContext;
+            this._userManager = userManager;
         }
 
 
-        public void Seed()
+        public async Task SeedAsync()
         {
+
+
             _cardContext.Database.EnsureCreated();
+
+
+            StoreUser user = await _userManager.FindByEmailAsync("domel13@gmail.com");
+
+            if (user == null)
+            {
+                user = new StoreUser
+                {
+                    FirstName = "Marcus",
+                    LstName = "Baaaaa",
+                    Email = "domel13@gmail.com",
+                    UserName = "domel13@gmail.com"
+                };
+
+                var result = await _userManager.CreateAsync(user, "P@assw0rd!");
+
+                if (result != IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Could not create new user");
+                }
+
+            }
             if (!_cardContext.PlayerCards.Any())
             {
 
@@ -34,18 +62,24 @@ namespace WebAppCard.Data.Seeder
                 _cardContext.PlayerCards.AddRange(cards);
 
 
-                var order = new Order()
+                //var order = new Order()
+                var order = _cardContext.Orders.Where(x => x.Id == 1).FirstOrDefault();
+
+                if (order != null)
                 {
-                    OrderDate = DateTime.Today,
-                    OrderNumber = 13547,
-                    Items = new List<OrderItem> {
+                    //OrderDate = DateTime.Today,
+                    //OrderNumber = 13547,
+                    //Items = new List<OrderItem> 
+                    order.User = user;
+                    order.Items = new List<OrderItem>
+                    {
                        new OrderItem()
                        {
                            PlayerCard = cards.First(),
                            Quantity = 13,
                            UnitPrice = cards.First().Price
                        }
-                    }
+                    };
                 };
 
                 _cardContext.Orders.Add(order);
